053a3034a33a385c4245de3382a2d9bc93b559b9
Clay Kaufmann <claykaufmann@gmail.com>
2022-06-24 10:35:54 -0400

    update setup.sh, add linux dotfiles

---
+++ linux/README.md
@@ -0,0 +1,4 @@
+# Linux Specific Dotfiles
+In here are my linux specific dotfiles. They are correctly installed with the general install script found in the main directory.
+
+## NOTE: I don't think the picom.conf in the main folder here is necessary, testing will be needed.
--- setup.sh
+++ setup.sh
@@ -1,6 +1,38 @@
 #! /bin/bash
 
 # detect OS, as different dotfiles should be installed depending on mac or linux
+# NOTE: This is not setup to work on windows AT ALL for now
+
 if [[ $OSTYPE == 'darwin'* ]]; then
-    echo 'macOS'
+    echo 'Detected macOS operating system, will install mac specific files.'
+fi
+
+# symlink stuff needed in both mac/linux
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "Installing files from the directory $SCRIPT_DIR"
+
+echo "Symlinking .gitconfig..."
+ln -s $SCRIPT_DIR/.gitconfig ~/.gitconfig
+
+echo "Symlinking Doom Emacs config..."
+ln -s $SCRIPT_DIR/doom ~/.config/doom
+
+echo "Symlinking kitty config..."
+ln -s $SCRIPT_DIR/kitty ~/.config/kitty
+
+echo "Symlinking nvim config..."
+ln -s $SCRIPT_DIR/nvim ~/.config/nvim
+
+ismac=0
+if [[ $OSTYPE == 'darwin'* ]]; then
+    echo 'Installing macOS specific files...'
+    ismac=1
+    echo "Symlinking .zshrc..."
+    # if this is on mac, we need to symlink specific stuff, namely zsh in the mac folder
+    ln -s $SCRIPT_DIR/mac/.zshrc ~/.zshrc
+
+else
+    ismac=0
+    echo "Installed linux specific files..."
 fi
