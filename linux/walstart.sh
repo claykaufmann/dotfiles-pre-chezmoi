bfcd61b49b5b6e90d2d24ec64bfa811b3dee65bb
Clay Kaufmann <claykaufmann@gmail.com>
2022-06-25 13:07:14 -0400

    add spicetify call to walstart script

--- linux/walstart.sh
+++ linux/walstart.sh
@@ -4,5 +4,8 @@ wal -i $HOME/Dropbox/bg-photos-linux
 # update firefox colors
 pywalfox update
 
+# update spicetify colors
+spicetify apply
+
 # update background image
 feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
