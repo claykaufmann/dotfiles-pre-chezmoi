8fe46b565bf61902bfb3a3152ab9847be6b0768c
Clay Kaufmann <claykaufmann@gmail.com>
2022-10-24 09:42:18 -0300

    change doom test profile name

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,4 +1,4 @@
 (("default"   . ((user-emacs-directory . "~/doom-emacs")))
   ("legacy" . ((user-emacs-directory . "~/.emacs.legacy")))
-  ("test-doom" . ((user-emacs-directory . "~/doom-emacs-test"))))
+  ("doom-test" . ((user-emacs-directory . "~/doom-emacs-test"))))
  
