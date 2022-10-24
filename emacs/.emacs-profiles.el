36280ddace9afa29711956d775cca2d597e2aede
Clay Kaufmann <claykaufmann@gmail.com>
2022-10-24 09:34:30 -0300

    add chemacs config file

---
+++ emacs/.emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default"   . ((user-emacs-directory . "~/doom-emacs")))
+  ("legacy" . ((user-emacs-directory . "~/.emacs.legacy")))
+  ("test-doom" . ((user-emacs-directory . "~/doom-emacs-test"))))
+ 
